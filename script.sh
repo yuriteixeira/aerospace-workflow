495bae31757e7dead8c167949616faca80357c29
Yuri Teixeira <oyuriteixeira@gmail.com>
2024-10-02 11:59:19 +0200

    feat: app icon on results list

--- script.sh
+++ script.sh
@@ -1,5 +1,30 @@
 #!/usr/bin/env bash
 
-windowsJson=$(aerospace list-windows --workspace visible --format "{ \"title\": \"%{app-name}\", \"subtitle\": \"%{window-title}\", \"arg\": \"%{window-id}\" },")
+# References:
+# - https://nikitabobko.github.io/AeroSpace/commands#list-windows
+# - https://www.alfredapp.com/help/workflows/inputs/script-filter/json/
+result=$( \
+  aerospace list-windows --workspace visible --format "%{app-pid} %{window-id} %{app-name}" | \
+  while read -r pid id name; \
+  do 
+    path="$(ps -o comm= -p "$pid")"
+    bundlePath="${path%%\.app*}.app"
 
-echo "{ \"items\": [ ${windowsJson} ] }" > ./windows.json
+    echo "{ 
+      \"title\": \"$name\", 
+      \"arg\": \"$id\", 
+      \"icon\": { 
+        \"type\": \"fileicon\", 
+        \"path\": \"$bundlePath\" 
+      } 
+    },";
+  done
+)
+
+echo "{ \"items\": [ $result ] }"
+
+if [ -n "$DEBUG" ]; then
+  echo ""
+  echo "--- END: $(date) ---"
+  echo ""
+fi
